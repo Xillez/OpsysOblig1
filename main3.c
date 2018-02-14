b2897d8eabd5a3104d32ff9ee8f3239fb4dc4f53
Kentwh <kentwh@stud.ntnu.no>
2018-02-14 15:42:18 +0100

    Fixed main3.c, made it run for limmited time, pluss gave full code privileges to erik, sturla and linnmari

--- main3.c
+++ main3.c
@@ -11,6 +11,8 @@
 #define HUNGRY 1
 #define EATING 2
 
+#define NR_PRINTS 10
+
 struct pargs
 {
     int nr;
@@ -20,42 +22,53 @@ pthread_t philosophers[N];
 int state[N];
 sem_t mutex;
 sem_t s[N];
+sem_t printing;
 
 void* philosopher(void* pargs);
 void take_forks(int i);
 void put_forks(int i);
 void test(int i);
 void eat(int i);
-void think(int i);
+void think();
 
 void eat(int i)
 {
+    // Print the states of all philosophers in a critical area
+    sem_wait(&printing);
+    for (int a = 0; a < N; a++)
+    {
+        printf("%s  ", ((state[a] == HUNGRY) ? "HUNGRY" : ((state[a] == THINKING) ? "THINKING" : "EATING")));
+    }
+    printf("\n");
+    sem_post(&printing);
+    
     // Eat for random amount of time
-    int t = rand() % 4;
-    //printf("Philosopher %d eats for: %d\n", i, t);
-    sleep(t);
-    //printf("Philosopher %d is done eating!\n", i);
+    sleep(rand() % 2);
 }
 
-void think(int i)
+void think()
 {
     // Think for random amount of time
-    sleep(rand() % 4);
+    sleep(rand() % 2);
 }
 
 void* philosopher(void* pargs)
 {
+    int nrPrints = 0;
+
     // Get arguments
     struct pargs* args = (struct pargs*) pargs;
 
-    while (1 == 1)
+    while (nrPrints < NR_PRINTS)
     {
-        think(args->nr);
+        think();
         take_forks(args->nr);
         eat(args->nr);
         put_forks(args->nr);
+        
+        nrPrints++;
     }
-    pthread_exit(0);
+    return 0;
 }
 
 void take_forks(int i)
@@ -96,12 +109,14 @@ void test(int i)
 
 int main()
 {
+    int i = 0;
     sem_init(&mutex, 0, 1);
+    sem_init(&printing, 0, 1);
     srand(time(NULL));
     //struct pargs* args[N];'
     struct pargs args[N];
     // Loop through the philosophers, init their semphore, and creates them
-    for (int i = 0; i < N; i++)
+    for (i = 0; i < N; i++)
     {
         // Define nr argument
         args[i].nr = i;
@@ -111,23 +126,10 @@ int main()
         pthread_create(&philosophers[i], NULL, philosopher, (void*) &args[i]);
     }
 
-    // Loop forever
-    while (1 == 1)
-    {
-        // Run through philosophers printing their state
-        for (int i = 0; i < N; i++)
-        {
-            printf("%s  ", ((state[i] == HUNGRY) ? "HUNGRY" : ((state[i] == THINKING) ? "THINKING" : "EATING")));
-        }
-        printf("\n");
-    }
-
     // Wait for all philosophers
     for (int i = 0; i < N; i++)
     {
         pthread_join(philosophers[i], NULL);
     }
     exit(0);
-
-    return 0;
 }
